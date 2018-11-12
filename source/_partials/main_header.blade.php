fb884681f7d9f94b6e38093a64d8387a5b25f4a8
Christian <christian@murphyvarley.com>
2018-11-12 17:26:37 +0000

    Set up new scss files. Create navbar and header partials

---
+++ source/_partials/main_header.blade.php
@@ -0,0 +1,3 @@
+<header class="website-header">
+    <h1 class="website-header__title">Front-End Web Developer</h1>
+</header>
--- source/_partials/navbar.blade.php
+++ source/_partials/navbar.blade.php
@@ -1,18 +1,26 @@
-<nav class="site-bar site-bar--theme-dark site-bar--border-bottom navbar navbar-expand-lg">
-    <a class="site-bar__logo" href="/">
-        <img src="/" width="150" alt="Logo">
-    </a>
-    <button class="site-bar__toggle-icon d-lg-none" type="button" data-toggle="collapse" data-target="#siteBar" aria-controls="siteBar" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fal fa-bars"></i>
-    </button>
-    <div id="siteBar" class="site-bar__links-wrapper collapse navbar-collapse justify-content-end">
-        <ul class="site-bar__links navbar-nav">
-            <li class="nav-item">
-                <a class="site-bar__link nav-link site-bar__link--active" href="#">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="site-bar__link nav-link" href="#">Contact</a>
-            </li>
-        </ul>
+<nav class="main-bar navbar navbar-expand-md navbar-light sticky-top">
+    <div class="container">
+        <a class="main-bar__logo-wrap navbar-brand" href="#">
+            <span class="main-bar__logo-text">Christian Nye</span>
+        </a>
+        <button class="main-bar__toggle navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="main-bar__toggler navbar-toggler-icon"></span>
+        </button>
+        <div class="main-bar__navigation collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav ml-auto">
+                <li class="main-bar__item nav-item active">
+                    <a class="main-bar__link nav-link" href="#">Home</a>
+                </li>
+                <li class="main-bar__item nav-item">
+                    <a class="main-bar__link nav-link" href="#">About</a>
+                </li>
+                <li class="main-bar__item nav-item">
+                    <a class="main-bar__link nav-link" href="#">Work</a>
+                </li>
+                <li class="main-bar__item nav-item">
+                    <a class="main-bar__link nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
